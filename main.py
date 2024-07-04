b65c36d8ab6a8ab025c183b9b033fe96aee4178b
Minseok Kwak <kwak@minseok.me>
2024-07-04 13:04:53 +0900

    핵

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-def example_function(a, b):
-    x = {"key1": "value1", "key2": "value2"}
-    if a > b:
-        print("a is greater than b")
+def example_function( a,b ):
+    x= {  'key1'  :    'value1', 'key2' :'value2'}
+    if( a> b  ):
+        print( "a is greater than b"  )
     else:
-        print("a is not greater than b")
-    for i in [1, 2, 3]:
-        print(i)
+     print(  "a is not greater than b"  )
+    for i in[1,2,3]:
+      print(  i)
 
-
-example_function(10, 5)
+example_function(  10 ,5  )
